0975fa2dbd904c80ace944dcd5d9d6d966c6540e
Boris 'billiob' Faure <billiob@gmail.com>
2009-12-01 23:37:11 +0100

    add a tool to generate C code from brainfuck

---
+++ bfutils/bf2c.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python2.6
+"""
+A brainfuck(++) translator to C.
+
+Copyright (C) 2009 Boris 'billiob' Faure
+This code is under the DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE Version 2
+
+
+TODO:
+* Do not harcode the path to bfpp.in.c
+"""
+import os
+import sys
+import optparse
+import bfpreprocessor
+
+def TranslateToC(codelist, file):
+    fin = open('bfutils/bfpp.in.c', 'r')
+    file.writelines(fin.readlines())
+    fin.close()
+    for (f,l,c) in codelist:
+        file.write('/* %s:%d */\n' % (f,l))
+        for op in c:
+            if op == '+':
+                file.write('++*ptr;                         /* + */\n')
+            elif op == '-':
+                file.write('--*ptr;                         /* - */\n')
+            elif op == '.':
+                file.write('putchar(*ptr);                  /* . */\n')
+            elif op == ',':
+                file.write('*ptr = getchar();               /* , */\n')
+            elif op == '<':
+                file.write('--ptr;                          /* < */\n')
+            elif op == '>':
+                file.write('++ptr;                          /* > */\n')
+            elif op == '[':
+                file.write('while (*ptr) {                  /* ] */\n')
+            elif op == ']':
+                file.write('}                               /* ] */\n')
+            elif op == '%':
+                file.write('bf_socket_open_close(r, &ptr);  /* % */\n')
+            elif op == '^':
+                file.write('bf_socket_send(r, &ptr);        /* ^ */\n')
+            elif op == '!':
+                file.write('bf_socket_read(r, &ptr);        /* ! */\n')
+            elif op == '#':
+                file.write('bf_file_open_close(r, &ptr);    /* # */\n')
+            elif op == ';':
+                file.write('bf_file_write(r, &ptr);         /* ; */\n')
+            elif op == ':':
+                file.write('bf_file_read(r, &ptr);          /* : */\n')
+            elif op == 'D':
+                file.write('bf_debug(r, &ptr);              /* D */\n')
+    file.write('return 0; }\n')
+    file.close()
+
+def main():
+    parser = optparse.OptionParser(usage="%prog [OPTIONS] FILE")
+
+    parser.add_option("-c",
+                      dest="cfilename", action="store", type="string",
+                      help="write to file a version translated to C")
+
+    parser.add_option("-d", "--debug",
+                      dest="debug", default=False, action="store_true",
+                      help=" in interactive debug mode when 'D' is encountered")
+    (options, args) = parser.parse_args()
+
+    filename = None
+    if len(args) != 1:
+        parser.error("incorrect number of arguments")
+    if args[0] != '-':
+        filename = os.path.abspath(args[0])
+
+    if options.cfilename is not None:
+        code = bfpreprocessor.preprocess2(filename, options.debug)
+        f = open(options.cfilename, 'w')
+        TranslateToC(code, f)
+
+if __name__ == '__main__':
+    sys.exit(main())
+
