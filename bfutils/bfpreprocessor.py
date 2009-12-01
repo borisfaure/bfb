e9ad4680e399b39781cb9633fb3d38c2861b6634
Boris 'billiob' Faure <billiob@gmail.com>
2009-12-01 23:35:31 +0100

    add a new preprocessor in order to improve the readability of the generated C code

--- bfutils/bfpreprocessor.py
+++ bfutils/bfpreprocessor.py
@@ -41,3 +41,41 @@ def preprocess(filename, debug = False):
             (line, sep, ignore) = line.partition('=')
             data.append(''.join(c for c in line if c in BFIS))
     return ''.join(data)
+
+
+def preprocess2(filename, debug = False):
+    """ Preprocess the brainfuck code, with debugging in mind
+        The object returned is a list of tuples (f, l, c) where:
+        f(string) current filename
+        l(int) current line number
+        c(list of char) brainfuck code of the current line
+    """
+    linenbr = 0
+    include_re = re.compile(r'\s*@include\(([^)]+)\)')
+    if debug:
+        BFIS = set([',','.','-','+','[',']','<','>','#',';',':','%','^','!','D'])
+    else:
+        BFIS = set([',','.','-','+','[',']','<','>','#',';',':','%','^','!'])
+    if filename:
+        file = open(filename, "r")
+        directory = os.path.split(filename)[0]
+    else:
+        filename = "STDIN"
+        file = sys.stdin
+        directory = os.getcwd()
+
+    data = []
+    for line in file:
+        if len(line) != 0:
+            m = include_re.match(line)
+            if m:
+                l = preprocess2(os.path.join(directory, m.group(1)), debug)
+                if l:
+                    data.extend(l)
+            else :
+                (line, sep, ignore) = line.partition('=')
+                ops = ''.join(c for c in line if c in BFIS)
+                if ops:
+                    data.append((filename, linenbr, ops))
+        linenbr = linenbr + 1
+    return data
